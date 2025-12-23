c6dfdb1cb466568060094430e92212996ff6dbbc
Shubrato Debnath <shubratodn44985@gmail.com>
2025-12-23 20:40:01 +0600

    View update

--- src/main/java/com/heal/io/HealioApplication.java
+++ src/main/java/com/heal/io/HealioApplication.java
@@ -2,9 +2,16 @@ package com.heal.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HealioApplication {
+public class HealioApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HealioApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealioApplication.class, args);
