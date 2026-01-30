f5e1b654665bf3a129a3316432d8d13f726c798b
Shubrato Debnath <shubratodn44985@gmail.com>
2026-01-30 20:22:19 +0600

    Addes sql

---
+++ truncate_sales_stock_inventory.sql
@@ -0,0 +1,31 @@
+-- PostgreSQL SQL Script to Truncate Sale, Stock, and Inventory Data
+-- This script will delete all data from sales, stock-in, and inventory tables
+-- while preserving product and dosage form information
+
+-- Disable foreign key checks temporarily (PostgreSQL doesn't have this, but we'll use CASCADE)
+-- Start a transaction for safety
+BEGIN;
+
+-- 1. Truncate Sale-related tables (child tables first, then parent)
+TRUNCATE TABLE sale_item CASCADE;
+TRUNCATE TABLE sale CASCADE;
+
+-- 2. Truncate Stock In-related tables (child tables first, then parent)
+TRUNCATE TABLE stock_in_item CASCADE;
+TRUNCATE TABLE stock_in CASCADE;
+
+-- 3. Truncate Inventory table
+TRUNCATE TABLE inventory CASCADE;
+
+-- Commit the transaction
+COMMIT;
+
+-- Alternative approach if you want to reset sequences as well:
+-- BEGIN;
+-- TRUNCATE TABLE sale_item RESTART IDENTITY CASCADE;
+-- TRUNCATE TABLE sale RESTART IDENTITY CASCADE;
+-- TRUNCATE TABLE stock_in_item RESTART IDENTITY CASCADE;
+-- TRUNCATE TABLE stock_in RESTART IDENTITY CASCADE;
+-- TRUNCATE TABLE inventory RESTART IDENTITY CASCADE;
+-- COMMIT;
+
